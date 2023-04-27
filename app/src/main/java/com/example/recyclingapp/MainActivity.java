9980e6cf1ffd2ea17d254c64370471054c442956
Robert Burns <robertcburns25@gmail.com>
2023-04-26 20:18:24 -0400

    Added basic camera functionality

--- app/src/main/java/com/example/recyclingapp/MainActivity.java
+++ app/src/main/java/com/example/recyclingapp/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.recyclingapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.content.Intent;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE2 = 17;
+    Button cameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraButton = findViewById(R.id.button_id);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(camera,REQUEST_CODE2);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
