e986a6703688d6099c036b47422956b663fe6dfc
Robert Burns <robertcburns25@gmail.com>
2023-05-17 01:55:40 -0400

    Added functionality to post image to server and receive results

--- TrashNet/app.py
+++ TrashNet/app.py
@@ -7,6 +7,10 @@ from torchvision.models import mobilenet_v3_large
 import torch.nn as nn
 import torchvision.transforms as transforms
 
+import base64
+from io import BytesIO
+import os
+
 app = Flask(__name__)
 class_names = ['cardboard', 'e-waste', 'glass', 'medical', 'metal', 'paper', 'plastic']
 
@@ -27,18 +31,19 @@ net.eval()
 #index
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.files['image']
-    img = Image.open(data)
+    data = request.form
+    im_bytes = base64.b64decode(data['image'])
+    im_file = BytesIO(im_bytes)
+    img = Image.open(im_file)   
     img = np.array(img)
     img = cv2.resize(img,(224,224))
     img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
     img = transform(img) # apply transformation
     img = img.unsqueeze(0)
-    
     logits = torch.softmax(net(img), dim=-1)
     pred_idx = torch.argmax(logits).item()
-
     return jsonify({"class": class_names[pred_idx]})
+    
 
 if __name__ == '__main__':
     app.secret_key = 'randomfkingkeyboiz'
--- app/src/main/java/com/example/recyclingapp/MainActivity.java
+++ app/src/main/java/com/example/recyclingapp/MainActivity.java
@@ -1,38 +1,140 @@
 package com.example.recyclingapp;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Bitmap;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.content.Intent;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
 
-import android.os.Bundle;
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_CODE2 = 17;
-    Button cameraButton;
+
+    //server address
+    private static final String SERVER_ADDRESS = "http://10.0.2.2:5000/predict";
+
+    ImageButton cameraButton;
+    ImageView imageView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        cameraButton = findViewById(R.id.button_id);
+        cameraButton = findViewById(R.id.imageButton);
+        textView = findViewById(R.id.resultText);
+        imageView = findViewById(R.id.imageview);
+
 
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivityForResult(camera,REQUEST_CODE2);
+
             }
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE2 && resultCode == RESULT_OK)
+        {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            textView.setText("");
+            imageView.setImageBitmap(photo);
+
+            new UploadImage(photo, "inputImage").execute();
+        }
+        else {
+            Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private class UploadImage extends AsyncTask<Void, Void, Void> {
+        Bitmap image;
+        String name;
+
+        public UploadImage(Bitmap image, String name){
+            this.image = image;
+            this.name = name;
+        }
+
+        public void displayResponse(String response) {
+            textView = findViewById(R.id.resultText);
+            String []splitterString=response.split("\"");
+            textView.setText(splitterString[3]);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+            String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+
+            ArrayList<NameValuePair> dataToSend = new ArrayList<>();
+            dataToSend.add(new BasicNameValuePair("image", encodedImage));
+            dataToSend.add(new BasicNameValuePair("name", name));
+
+            HttpParams httpRequestParams = getHttpRequestParams();
+
+            HttpClient client = new DefaultHttpClient(httpRequestParams);
+            HttpPost post = new HttpPost(SERVER_ADDRESS);
+            HttpResponse response;
+
+            try {
+                post.setEntity(new UrlEncodedFormEntity(dataToSend));
+                response = client.execute(post);
+                HttpEntity entity = response.getEntity();
+                displayResponse(EntityUtils.toString(entity));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            Toast.makeText(getApplicationContext(), "Image Uploaded", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private HttpParams getHttpRequestParams(){
+        HttpParams httpRequestParams = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(httpRequestParams, 1000 * 300);
+        HttpConnectionParams.setSoTimeout(httpRequestParams, 1000*300);
+        return httpRequestParams;
     }
 }
+
